8f0f190504c49571d94d262de536210fd2357a3f
Sharp0802 <sharp0802.business@gmail.com>
2024-08-04 22:26:59 +0900

    :white_check_mark: Add test code

---
+++ test/src/main.cxx
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <frxml.h>
+
+int main()
+{
+    std::string xml = R"(
+<xml0>
+    <xml1 attr="0asdfas" attr0="1"><xml3 />
+<!-- HELLO! -->
+    </xml1>
+    <?test-pcinstr Hello World!?>
+    <xml2 attr="c"/>
+</xml0>
+)";
+    frxml::doc doc{ xml };
+    if (!doc)
+    {
+        auto error = doc.error();
+        std::cout << std::string_view(error.source - 1, 3) << ',' << error.code << '\n';
+    }
+
+    std::cout << static_cast<std::string>(doc);
+}
