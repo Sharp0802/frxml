83ffe3f32baaeb8f5c34f974806e4d1b44b7f521
Sharp0802 <sharp0802.business@gmail.com>
2024-10-11 15:41:37 +0900

    :white_check_mark: Add benchmark

--- test/src/main.cxx
+++ test/src/main.cxx
@@ -1,44 +1,108 @@
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
 #include <frxml.h>
+#include <libxml2/libxml/parser.h>
+#include <pugixml.hpp>
+#include <ranges>
+#include <algorithm>
+
+timespec diff(timespec start, timespec end)
+{
+    timespec temp{};
+    if (end.tv_nsec - start.tv_nsec < 0)
+    {
+        temp.tv_sec  = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
+    else
+    {
+        temp.tv_sec  = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return temp;
+}
+
+double todouble(timespec ts)
+{
+    ts.tv_nsec /= 100;
+    return static_cast<double>(ts.tv_sec) + static_cast<double>(ts.tv_nsec) / 10000000.0;
+}
+
+double benchmark(void (*fn)())
+{
+    timespec start{}, end{};
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+
+    for (int i = 0; i < 1000; ++i) fn();
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+
+    return todouble(diff(start, end));
+}
 
 int main()
 {
+    static int     code = 0;
+    static char    buf[1048576];
+    static ssize_t size;
+
+    std::cout << "Size (KB), Depth, FRXML (ms), LibXml2 (DOM, ms), PugiXML (ms)" << std::endl;
+
+    for (std::string file: {
+             "1, 3",
+             "1, 10",
+             "10, 3",
+             "10, 10",
+             "50, 3",
+             "50, 10"
+         })
     {
-        std::string xml = R"(
-<?xml version="1.0" encoding="UTF-8"?>
-<?ユニコードは 好きですか??>
-<!-- HELLO! -->
-<xml0>
-    <유니코드 attr="안녕하세요" attr0="1"><xml3 />
-<!-- HELLO! -->
-    </유니코드>
-    <?ユニコードは 好きですか??>
-    <xml2 attr="c"/>
-</xml0>
-<!-- HELLO! -->
-)";
-        frxml::doc doc{ xml };
-        if (!doc)
+        const auto fd = open((file + ".xml").c_str(), O_RDONLY);
+        if (fd < 0)
         {
-            auto [code, source] = doc.error();
-            std::cout << std::string_view(source - 1, 3) << ',' << code << '\n';
+            perror("open");
+            continue;
         }
-        //doc.children().push_back(frxml::dom::element("added-elem"));
 
-        std::cout << static_cast<std::string>(doc);
-    }
-    std::cout << "================================\n";
-    {
-        auto root = frxml::dom::element("xml0", {
-            { "xmlns", "test-xmlns" }
-        }, {
-            frxml::dom::comment(" Hello, FRXML! "),
-            frxml::dom::pcinstr("xml1", "유니코드 유니코드"),
-            frxml::dom::element("test-element")
+        size                = read(fd, buf, sizeof buf);
+        buf[sizeof buf - 1] = 0;
+
+        auto frxml = benchmark([]
+        {
+            if (const frxml::doc doc{ std::string_view(buf, size) }; !doc)
+                code = doc.error().code;
         });
-        root.children().push_back(frxml::dom::element("added-elem"));
 
-        frxml::doc doc{ { root } };
-        std::cout << static_cast<std::string>(doc);
+        double libxml;
+        if (size > 1024)
+        {
+            libxml = -1;
+        }
+        else
+        {
+            libxml = benchmark([]
+            {
+                auto doc = xmlReadMemory(buf, size, nullptr, nullptr, 0);
+                xmlFreeDoc(doc);
+                xmlCleanupParser();
+            });
+        }
+
+        auto pugixml = benchmark([]
+        {
+            pugi::xml_document doc;
+            [[maybe_unused]]
+                pugi::xml_parse_result result = doc.load_string(buf, pugi::parse_default | pugi::encoding_utf8);
+        });
+
+        std::cout << file << ", " << frxml << ", " << libxml << ", " << pugixml << std::endl;
+
+        if (code != 0)
+            std::cerr << "Exited with code: " << code << std::endl;
+
+        close(fd);
     }
+
+    return 0;
 }
