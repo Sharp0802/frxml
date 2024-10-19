a2167c10a9e74d61546c48bb26187f55cec424b9
Sharp0802 <sharp0802.business@gmail.com>
2024-10-20 00:54:22 +0900

    :sparkles: Update visualiser

--- test/script/main.py
+++ test/script/main.py
@@ -14,13 +14,19 @@ for i in range(len(df["name"])):
     df.at[i, "name"] = terms[0].split("_")[1]
     df.at[i, "count"] = terms[1]
 
-df.sort_values(by="count", inplace=True)
+relative_df = df.copy()
+for i in range(len(df)):
 
-df = df[['name', 'count', 'real_time', 'max_bytes_used']]
+    origin_real_time = df[(df['name'] == 'frxml') & (df['count'] == df.at[i, 'count'])]['real_time'].values[0]
+    relative_df.at[i, 'real_time'] /= origin_real_time
+
+    origin_max_bytes_used = df[(df['name'] == 'frxml') & (df['count'] == df.at[i, 'count'])]['max_bytes_used'].values[0]
+    relative_df.at[i, 'max_bytes_used'] /= origin_max_bytes_used
+
+df = relative_df
 
 df['count'] = df['count'].astype(float) / 1024
-df['real_time'] = df['real_time'].astype(float) / 1000000
-df['max_bytes_used'] = df['max_bytes_used'].astype(float) / 1024
+
 
 perf_pivot = df.pivot_table(index='count', columns='name', values='real_time')
 mem_pivot = df.pivot_table(index='count', columns='name', values='max_bytes_used')
@@ -35,26 +41,33 @@ fig, axes = plt.subplots(nrows=1, ncols=2)
 plt.sca(axes[0])
 
 plt.xlabel("XML Size (KiB)")
-plt.ylabel("Elapsed (ms)")
+plt.ylabel("Elapsed (relative to frxml)")
+plt.xlim([perf_pivot['count'].min(), perf_pivot['count'].max()])
 
-for lib in ['pugixml', 'tinyxml2', 'rapidxml', 'frxml']:
+for lib in ['pugixml', 'tinyxml2', 'rapidxml', 'libxml2', 'frxml']:
     plt.plot(perf_pivot['count'], perf_pivot[lib], label=lib)
 
 plt.xticks(rotation=45, ha='right')
-plt.title("Parsing Time")
+plt.semilogy()
+plt.yticks([ 0.5, 1, 2, 3, 4, 5 ], [ '0.5', '1.0', '2.0', '3.0', '4.0', '5.0' ])
+plt.title("Parsing Time (log scale)")
 plt.legend(title="Library")
 
 
 plt.sca(axes[1])
 
+axes[1].set_aspect('auto')
 plt.xlabel("XML Size (KiB)")
-plt.ylabel("Usage (KiB)")
+plt.ylabel("Usage (relative to frxml)")
+plt.xlim([mem_pivot['count'].min(), mem_pivot['count'].max()])
+#plt.ylim([df['max_bytes_used'].min(), 3])
 
-for lib in ['pugixml', 'tinyxml2', 'rapidxml', 'frxml']:
+for lib in ['pugixml', 'tinyxml2', 'rapidxml', 'libxml2', 'frxml']:
     plt.plot(mem_pivot['count'], mem_pivot[lib], label=lib)
 
 plt.xticks(rotation=45, ha='right')
-plt.title("Allocated Memory")
+plt.yticks([ 0.5, 1, 2, 3, 4, 5 ], [ '0.5', '1.0', '2.0', '3.0', '4.0', '5.0' ])
+plt.title("Allocated Memory (linear scale)")
 plt.legend(title="Library")
 
 fig.tight_layout()
