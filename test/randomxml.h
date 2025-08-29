d8b536614d509d1327fececbdc6e9a309f5f6247
Sharp0802 <sharp0802.business@gmail.com>
2025-08-30 08:57:06 +0900

    :fire: Remove test

--- test/randomxml.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <random>
-#include <string>
-#include <pugixml.hpp>
-
-std::string RandomString(int length);
-void GenerateXML(pugi::xml_node& parent, int& node_count, int depth);
