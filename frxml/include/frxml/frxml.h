44c8b029687af8957994478b0cef9f1683751dec
Sharp0802 <sharp0802.business@gmail.com>
2025-08-30 09:10:28 +0900

    :hammer: Make it header-only library

--- frxml/include/frxml/frxml.h
+++ frxml/include/frxml/frxml.h
@@ -179,6 +179,48 @@ namespace frxml {
    * Non-zero if parsing succeeded; otherwise, zero.
    */
   FRXML_EXPORT const element *parse(std::string_view str, const std::vector<uint8_t> &buffer);
+
+  namespace details {
+    inline node_type operator&(node_type lhs, node_type rhs) {
+      const auto l = static_cast<std::underlying_type_t<node_type>>(lhs);
+      const auto r = static_cast<std::underlying_type_t<node_type>>(rhs);
+      return static_cast<node_type>(l & r);
+    }
+
+    inline const void *skip_node(const void *p) {
+      const auto t = *static_cast<const node_type*>(p);
+      if ((t & node_type::ELEMENT) == node_type::ELEMENT) {
+        return static_cast<const uint8_t*>(p) + sizeof(element);
+      }
+
+      switch (t) {
+      case node_type::ATTR:
+        return static_cast<const uint8_t*>(p) + sizeof(attr);
+      case node_type::END_OF_ELEMENT:
+        return static_cast<const uint8_t*>(p) + sizeof(end);
+      default:
+        return nullptr;
+      }
+    }
+  }
+
+  inline node_iterator<element> element::children() const {
+    using namespace details;
+
+    const void *next = this;
+    do {
+      next = skip_node(next);
+    } while (*static_cast<const node_type*>(next) == node_type::ATTR);
+
+    return node_iterator{static_cast<const element*>(next)};
+  }
+
+  inline node_iterator<attr> element::attrs() const {
+    using namespace details;
+    
+    const void *next = skip_node(this);
+    return node_iterator{static_cast<const attr*>(next)};
+  }
 }
 
 #endif //FRXML_LIBRARY_H
