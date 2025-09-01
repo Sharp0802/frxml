dfefef626b3ec86f05bff756487fec4dcaf48dae
Sharp0802 <sharp0802.business@gmail.com>
2025-09-01 11:28:12 +0900

    :recycle: Refine semantics

--- frxml/include/frxml/string.h
+++ frxml/include/frxml/string.h
@@ -5,12 +5,18 @@
 #include <string_view>
 #include <utility>
 
+#ifdef SPACE
+#warning 'SPACE' macro is already occupied; Result may malfunction.
+#else
+#define SPACE ' ', '\t', '\r', '\n'
+#endif
+
 namespace frxml::details {
-  template <char... chs>
+  template<char... chs>
   using tstring = std::integer_sequence<char, chs...>;
 
-  template <typename T, T... chs>
-  constexpr tstring<chs...> operator""_t() { return { }; }
+  template<typename T, T... chs>
+  constexpr tstring<chs...> operator""_t() { return {}; }
 
   class view {
     const char *_begin;
@@ -29,7 +35,10 @@ namespace frxml::details {
     }
 
     [[nodiscard]]
-    view substr(size_t i, size_t size) const;
+    const char *begin() const { return _begin; }
+
+    [[nodiscard]]
+    const char *end() const { return _end; }
 
     template<char... chs>
     [[nodiscard]]
@@ -48,25 +57,24 @@ namespace frxml::details {
     }
 
     template<char... chs>
-    void until();
+    void skip_until();
 
     template<char... chs>
-    void until(tstring<chs...>);
+    void skip_until(tstring<chs...>);
+
+    template<size_t N>
+    void forward();
 
     [[nodiscard]]
     bool eof() const {
       return _begin == _end;
     }
-  };
 
-  inline view view::substr(const size_t i, const size_t size) const {
-#if _DEBUG
-    if (_begin + i + size > _end) {
-      throw std::out_of_range("string::substr() out of range");
+    [[nodiscard]]
+    std::string_view to_std() const {
+      return {_begin, static_cast<std::size_t>(_end - _begin)};
     }
-#endif
-    return {_begin + i, _begin + i + size};
-  }
+  };
 
   template<char... chs>
   bool view::start_with() const {
@@ -98,14 +106,21 @@ namespace frxml::details {
   }
 
   template<char... chs>
-  void view::until() {
+  void view::skip_until() {
     for (; !eof() && !start_with<chs...>(); ++_begin) {
     }
   }
 
   template<char... chs>
-  void view::until(tstring<chs...> t) {
+  void view::skip_until(tstring<chs...> t) {
     for (; !eof() && !start_with(t); ++_begin) {
     }
+  }
+
+  template<size_t N>
+  void view::forward() {
+    _begin += N;
+    if (_begin > _end)
+      _begin = _end;
   }
 }
